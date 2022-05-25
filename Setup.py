feee873ea4638a8a135fb3e018b73bbe8759cb2f
jess-mkg <78837964+jess-mkg@users.noreply.github.com>
2022-05-25 12:53:49 +1000

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,12 @@
+
+
+import pandas as pd
+
+class SetUp: 
+
+    def __init__(self, file, types):
+        self.df = self.load_csv(file, types)
+
+    def load_csv(self, file, types):
+        self.df = pd.read_csv(file, parse_dates = True, dtype = types)
+        return self.df
